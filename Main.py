b8d01a7242f1aa0f633b5ecd948ec6464a147425
Sashadudkin <sasha.dudkin@stu.ocsb.ca>
2026-09-08 11:05:13 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 #############################################
-# Name: Your name
+# Name: Sasha Dudkin
 # Class: ICS3C
 # Date: When It's Due
 # Project Name:
@@ -8,3 +8,4 @@
 #############################################
 
 # THIS IS WHERE YOU CODE
+print("I hope this works!")
